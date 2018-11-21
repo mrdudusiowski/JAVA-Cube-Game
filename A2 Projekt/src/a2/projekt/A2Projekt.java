b4a81bbfbcd7c10b6a6fd36e6e612790e7d528f3
mrdudusiowski <33097441+mrdudusiowski@users.noreply.github.com>
2018-11-21 14:47:14 +0100

    V3
    Dodanie multiplayera, usuniecie playera

--- A2 Projekt/src/a2/projekt/A2Projekt.java
+++ A2 Projekt/src/a2/projekt/A2Projekt.java
@@ -1,9 +1,5 @@
 package a2.projekt;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class A2Projekt {
 
     public static void main(String[] args) {
@@ -11,30 +7,18 @@ public class A2Projekt {
 //      Deklaracja zmiennych
         Game game = new Game();
         Player player = new PlayerComp("Jan");     //PlayerHuman lub PlayerComp
-        List<String> list = new ArrayList();
 
 //      Powitanie
         System.out.println("Witamy, właśnie losujemy liczbę, zgadnij ją:\n\n");
 
-        list.add("raz");
-        list.add("dwa");
-        list.add("trzy");
-
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
-
-        for (String o : list) {
-            System.out.println(o);
-        }
-        
-        Iterator<String> it= list.iterator();
-        
-        while(it.hasNext()){
-        System.out.println(it.next());
-        }
-
         game.addPlayer(player);
+        game.addPlayer(new PlayerComp("Zdzichu"));
+        game.addPlayer(new PlayerComp("Alek"));
+        game.addPlayer(new PlayerComp("Jarek"));
+        game.printPlayers();
+        game.removePlayer("Jan");
+        game.printPlayers();
+        
         game.play();
 
     }
@@ -53,5 +37,29 @@ Wyrażenia regularne - regex101.com ^[a-zA-Z][a-zA-Z0-9@\-_.]{2,29}$
         } catch (IllegalArgumentException ex) {
             System.err.println("Błąd, źle wprowadzone dane! " + ex.getMessage());
 
+        }
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+ List<String> list = new ArrayList();
+ list.add("raz");
+        list.add("dwa");
+        list.add("trzy");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        for (String o : list) {
+            System.out.println(o);
+        }
+        
+        Iterator<String> it= list.iterator();
+        
+        while(it.hasNext()){
+        System.out.println(it.next());
         }
  */
